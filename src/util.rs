2badcc300fbd41e56a42e370b73f652493b15386
phosfox <mordredur@googlemail.com>
2019-08-19 00:58:14 +0200

    adds another todo to the testcase

--- src/util.rs
+++ src/util.rs
@@ -13,11 +13,13 @@ mod tests {
     #[test]
     fn test_write_todos(){
         let todo = TodoItem::new(String::from("test"), false);
+        let todo2 = TodoItem::new(String::from("test2"), true);
         let mut todos: Vec<TodoItem> = vec![];
         todos.push(todo);
+        todos.push(todo2);
 
         let mut result = Vec::new();
         write_todos(todos, &mut result);
-        assert_eq!(result, b"test,false\n");
+        assert_eq!(result, b"test,false\ntest2,true\n");
     }
 }
