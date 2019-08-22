c81a37b5735cfeb2ba3ae3ada13005667c01658f
phosfox <mordredur@googlemail.com>
2019-08-22 16:14:08 +0200

    wtf is this

--- src/util.rs
+++ src/util.rs
@@ -3,6 +3,18 @@ extern crate csv;
 use crate::todo_item::TodoItem;
 use std::io;
 use std::process;
+use std::str::FromStr;
+use std::num;
+
+enum ParseBoolError {
+    IoError(io::Error)
+}
+impl From<io::Error> for ParseBoolError {
+    fn from(error: io::Error) -> Self {
+        ParseBoolError::IoError(error)
+    }
+    
+}
 
 pub fn write_todos(todos: &Vec<TodoItem>, mut to: impl std::io::Write){
     for todo in todos.iter() {
@@ -24,7 +36,7 @@ pub fn print_todos(todos: &Vec<TodoItem>) {
     }
 }
 
-pub fn parse_csv(path: &std::path::Path) -> Result<Vec<TodoItem>, Box<io::Error>> {
+pub fn parse_csv(path: &std::path::Path) -> Result<Vec<TodoItem>, io::Error> {
     let mut reader = match csv::ReaderBuilder::new()
     .has_headers(false)
     .from_path(path) {
@@ -32,17 +44,28 @@ pub fn parse_csv(path: &std::path::Path) -> Result<Vec<TodoItem>, Box<io::Error>
         Ok(reader) => reader,
     };
 
-    let todos: Vec<TodoItem> = vec![];
+    let mut todos: Vec<TodoItem> = vec![];
 
     for result in reader.records() {
         match result {
             Err(err) => return Err(From::from(err)),
             Ok(record) => {
-                todos.push(TodoItem::new(record.get(0), record.get(1)));
+                let name: String = match record.get(0) {
+                    Some(name) => name.to_string(),
+                    None => panic!("name could not be parsed"),
+                };
+                
+                let done: bool = match record.get(1) {
+                    Some(done) => FromStr::from_str(done)?,
+                    None => panic!("done could not be parsed"),
+                };
+
+                todos.push(TodoItem::new(name, done));
             }
         }
     }
-    return todos;
+
+    return Ok(todos);
 }
 
 #[cfg(test)]
@@ -53,11 +76,15 @@ mod tests {
     fn test_parse_csv() {
         //let prop_todos = vec![TodoItem::new(String::from("test1"), false), TodoItem::new(String::from("test2"), false), TodoItem::new(String::from("test3"), false)];
 
-        let path = std::path::Path::new("../tests/props/parse_prob.csv");
-        let got_todos: Vec<TodoItem> = parse_csv(path);
-
-        assert_eq!(got_todos.len(), 3);
+        let path = std::path::Path::new("./tests/props/parse_prob.csv");
+        
+        let todos = match parse_csv(&path){
+            Err(err) => panic!("test failed because: {}", err),
+            Ok(todos) => todos,
+        };
+        assert_eq!(todos.len(), 3);
     }
+
     #[test]
     fn test_todos_to_csv() {
         let todo = TodoItem::new(String::from("test"), false);
