aa507ffac117988cdbe58fdf802538fb5e6e95df
phosfox <mordredur@googlemail.com>
2019-08-23 17:38:43 +0200

    removed deadcode

---
+++ TODO.md
@@ -0,0 +1,3 @@
+#Features that I still need to implement:
+ - [ ] possibility to change todo items (name and doneness)
+ - [ ] filter todo items by doneness (only done or undone)
--- src/util.rs
+++ src/util.rs
@@ -3,12 +3,6 @@ extern crate csv;
 use crate::todo_item::TodoItem;
 use std::io::Error;
 
-pub fn write_todos(todos: &Vec<TodoItem>, mut to: impl std::io::Write){
-    for todo in todos.iter() {
-        writeln!(to, "{},{}", todo.name, todo.done).unwrap();
-    }
-}
-
 pub fn todos_to_csv(todos: &Vec<TodoItem>) -> String {
     let mut todo_as_strings = Vec::new();
     for todo in todos.iter() {
@@ -66,17 +60,5 @@ mod tests {
 
         let csv = todos_to_csv(&todos);
         assert_eq!(csv, "test,false\ntest2,true")
-    }
-    #[test]
-    fn test_write_todos(){
-        let todo = TodoItem::new(String::from("test"), false);
-        let todo2 = TodoItem::new(String::from("test2"), true);
-        let mut todos: Vec<TodoItem> = vec![];
-        todos.push(todo);
-        todos.push(todo2);
-
-        let mut result = Vec::new();
-        write_todos(&todos, &mut result);
-        assert_eq!(result, b"test,false\ntest2,true\n");
     }
 }
